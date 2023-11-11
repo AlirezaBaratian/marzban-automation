c4575a7ed27a114606d84c9a473c08bd60a31103
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-11-11 13:34:25 +0330

    fix: Edit script guide

--- main/block_iranian_hosts.sh
+++ main/block_iranian_hosts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 clear
-echo "🚫 This script installs assets for blocking Iranian hosts."
+echo "This script installs assets for blocking Iranian hosts."
 sleep 2
 
 config() {
@@ -10,7 +10,9 @@ config() {
     wget -O /var/lib/marzban/assets/geoip.dat https://github.com/v2fly/geoip/releases/latest/download/geoip.dat
     wget -O /var/lib/marzban/assets/iran.dat https://github.com/bootmortis/iran-hosted-domains/releases/latest/download/iran.dat
 
-    echo "✅ Done!\n🧢Now, uncomment XRAY_PATH environment variable"
+    echo "Done! now visit the docs to setup your routing rules:
+    https://github.com/Gozargah/Marzban/discussions/38
+    "
 }
 
 config
