7add6f5b9e81cf76e589813af963b23e49418309
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-11-11 13:40:28 +0330

    refactor: Rewrite code as functions

--- main/install.sh
+++ main/install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-clear
-echo "💂🏼‍♀️ We're about to install Marzban on your Ubuntu server using Docker."
+function main() {
+    clear
+    echo "💂🏼‍♀️ We're about to install Marzban on your Ubuntu server using Docker."
 
-sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" @ install
+    sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" @ install
+}
+
+main
