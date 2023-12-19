f861621f70899c602ffbbc5e9148ef10950ee633
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-12-19 16:16:30 +0330

    fix: fix main install

--- main/install.sh
+++ main/install.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 function main() {
-    clear
-    echo "💂🏼‍♀️ We're about to install Marzban on your Ubuntu server using Docker."
-
     sudo bash -c "$(curl -sL https://github.com/Gozargah/Marzban-scripts/raw/master/marzban.sh)" @ install
 }
 
