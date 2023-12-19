7d27bf28fcaf3dea75ac080d6aa9d3635d14e1e4
Alireza Baratian <92058984+AlirezaBaratian@users.noreply.github.com>
2023-12-19 21:31:00 +0330

    fix: change to install

--- main/setup_warp.sh
+++ main/setup_warp.sh
@@ -19,31 +19,30 @@ install_wgcf() {
     ln -s /opt/wgcf/wgcf /usr/bin/
 }
 
-setup_wgcf() {
-    echo -e "\n" | wgcf register
-    sleep 5
-    account_config="wgcf-account.toml"
-    raed -p "Enter your WARP+ key: " warp_key
-    sed -i "/license_key/c\license_key = '$warp_key'" $account_config
-    wgcf update
-    sleep 3
-    wgcf generate
-    sleep 3
-}
+# setup_wgcf() {
+#     echo -e "\n" | wgcf register
+#     sleep 5
+#     account_config="wgcf-account.toml"
+#     raed -p "Enter your WARP+ key: " warp_key
+#     sed -i "/license_key/c\license_key = '$warp_key'" $account_config
+#     wgcf update
+#     sleep 3
+#     wgcf generate
+#     sleep 3
+# }
 
-update_xray_config() {
-    account_profile="wgcf-profile.toml"
-    private_key=$(cat $account_profile | grep "PrivateKey = " | awk ' {print $3} ')
-    xray_config="/var/lib/marzban/xray_config.json"
-    jq --arg private_key "$private_key" '.outbounds[1].settings.secretKey = $private_key' $xray_config > $xray_config
-    marzban restart
-}
+# update_xray_config() {
+#     account_profile="wgcf-profile.toml"
+#     private_key=$(cat $account_profile | grep "PrivateKey = " | awk ' {print $3} ')
+#     xray_config="/var/lib/marzban/xray_config.json"
+#     jq --arg private_key "$private_key" '.outbounds[1].settings.secretKey = $private_key' $xray_config > $xray_config
+#     marzban restart
+# }
 
 function main() {
     get_warp_key
     install_dependencies
     install_wgcf
-    setup_wgcf
 }
 
 main
